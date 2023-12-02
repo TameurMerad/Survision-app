9eff8f2426ca0f2b584289f4d38970404717e2af
Soufyan <tameur.merad@gmail.com>
2023-12-02 06:04:27 +0100

    login page add

--- app/src/main/java/com/example/survisionapp/MainActivity.kt
+++ app/src/main/java/com/example/survisionapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.survisionapp.auth_pages.Login
 import com.example.survisionapp.auth_pages.Register
 import com.example.survisionapp.ui.theme.SurvisionappTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
                 val colorStatusBar = Color(ContextCompat.getColor(this,R.color.orange_main))
                 rememberSystemUiController().setStatusBarColor(colorStatusBar)
-                Register()
+                Login()
         }
     }
 }
--- app/src/main/java/com/example/survisionapp/auth_pages/LoginPage.kt
+++ app/src/main/java/com/example/survisionapp/auth_pages/LoginPage.kt
@@ -1,4 +1,86 @@
 package com.example.survisionapp.auth_pages
 
-class LoginPage {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.survisionapp.R
+
+@Composable
+fun Login (){
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(15.dp)
+        .background(Color.White),
+
+        contentAlignment = Alignment.Center
+    ){
+        Column {
+            AuthTextField(  "Email")
+            Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+            AuthTextField(  "Password")
+            Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+            Text(
+                modifier = Modifier.fillMaxWidth().padding(start = 5.dp),
+                text = "Forgot your password ?",
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.sp,
+                color = Color(0xFFF26E23),
+                textAlign = TextAlign.Start
+
+                )
+            Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+
+            SignButton(text = "Sign In")
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(15.dp),
+        contentAlignment = Alignment.TopCenter
+    ){
+
+        Spacer(modifier = Modifier.height(25.dp))
+        Image(painter = painterResource(
+            id = R.drawable.logo_with_type),
+            contentDescription = "",
+            Modifier
+                .fillMaxWidth()
+                .padding(25.dp)
+                .size(127.dp, 27.dp)
+        )
+
+
+    }
+
+
 }
